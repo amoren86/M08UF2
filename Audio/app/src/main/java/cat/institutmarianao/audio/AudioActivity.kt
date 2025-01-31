460a8bf944e35a317e0e6f9c3a10287bd898eab6
amoren86 <amoren86@xtec.cat>
2025-01-31 16:59:54 +0100

    2025.01.31

---
+++ Audio/app/src/main/java/cat/institutmarianao/audio/AudioActivity.kt
@@ -0,0 +1,11 @@
+package cat.institutmarianao.audio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AudioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio)
+    }
+}
