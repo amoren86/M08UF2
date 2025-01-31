df1c47d4ac7f7281d8b06a0def202c5ef8175c12
Toni <65610749+amoren86@users.noreply.github.com>
2025-01-31 17:05:11 +0100

    Update AudioActivity.kt

--- Audio/app/src/main/java/cat/institutmarianao/audio/AudioActivity.kt
+++ Audio/app/src/main/java/cat/institutmarianao/audio/AudioActivity.kt
@@ -1,11 +1,35 @@
 package cat.institutmarianao.audio
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class AudioActivity : AppCompatActivity() {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
+import android.widget.Button
+import android.widget.TextView
+
+class PlayerActivity : AppCompatActivity() {
+    private lateinit var tvStatus: TextView
+    private lateinit var bPlay: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_audio)
+        setContentView(R.layout.activity_player)
+
+        tvStatus = findViewById(R.id.tv_status)
+        bPlay = findViewById(R.id.b_play)
+
+        val player = MediaPlayer.create(this, R.raw.letitbe)
+
+        bPlay.setOnClickListener {
+            if (player.isPlaying) {
+                player.stop()
+                player.prepare()
+            }
+            player.start()
+            tvStatus.text = "Playing..."
+            player.setOnCompletionListener {
+                tvStatus.text = "End of play"
+            }
+        }
     }
 }
